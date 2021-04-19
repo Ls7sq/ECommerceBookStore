327f4f5d4af22fc784102cbba358eacded44dfae
Ls7sq <lsq9388@gmail.com>
2021-04-19 08:52:15 +0800

    Add project files.

---
+++ BookStore/Migrations/20210419003938_CreateDataBase.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BookStore.Migrations
+{
+    public partial class CreateDataBase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Books",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Desc = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    UnitPrice = table.Column<double>(type: "float", nullable: false),
+                    Image = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Tags = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Books", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Carts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CustomerId = table.Column<int>(type: "int", nullable: false),
+                    CreationTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CheckoutTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    IsCheckOut = table.Column<bool>(type: "bit", nullable: false),
+                    Quantity = table.Column<int>(type: "int", nullable: false),
+                    Value = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Carts", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Customers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customers", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CartItems",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CartId = table.Column<int>(type: "int", nullable: false),
+                    BookId = table.Column<int>(type: "int", nullable: false),
+                    Quantity = table.Column<int>(type: "int", nullable: false),
+                    CheckoutTime = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CartItems", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_CartItems_Books_BookId",
+                        column: x => x.BookId,
+                        principalTable: "Books",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_CartItems_Carts_CartId",
+                        column: x => x.CartId,
+                        principalTable: "Carts",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Sessions",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(36)", maxLength: 36, nullable: false),
+                    CustomerId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Sessions", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Sessions_Customers_CustomerId",
+                        column: x => x.CustomerId,
+                        principalTable: "Customers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Wishlist_AJAX",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    WishlistStatus = table.Column<bool>(type: "bit", nullable: false),
+                    wishlistID = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    itemId = table.Column<int>(type: "int", nullable: true),
+                    CustomerId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Wishlist_AJAX", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Wishlist_AJAX_Books_itemId",
+                        column: x => x.itemId,
+                        principalTable: "Books",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Wishlist_AJAX_Customers_CustomerId",
+                        column: x => x.CustomerId,
+                        principalTable: "Customers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PurcahsedActivationCodes",
+                columns: table => new
+                {
+                    ActivationCode = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    CartItemId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PurcahsedActivationCodes", x => x.ActivationCode);
+                    table.ForeignKey(
+                        name: "FK_PurcahsedActivationCodes_CartItems_CartItemId",
+                        column: x => x.CartItemId,
+                        principalTable: "CartItems",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Books",
+                columns: new[] { "Id", "Desc", "Image", "Name", "Tags", "UnitPrice" },
+                values: new object[,]
+                {
+                    { 1, "Brings powerful charting capabilities to your .NET applications.", "/images/Charts.png", ".NET Charts", "power | chart | application", 99.0 },
+                    { 2, "Integrate your .NET apps with PayPal the easy way!", "/images/PayPal.png", ".NET PayPal", "integrate | pay | app", 69.0 },
+                    { 3, "Supercharged .NET machine learning libraries.", "/images/ML.png", ".NET ML", "machine | learn | ml", 299.0 },
+                    { 4, "Performs data mining and analytics easily in .NET.", "/images/Analytics.png", ".NET Analytics", "data | mining | analytic", 299.0 },
+                    { 5, "Logs and aggregates events easily in your .NET apps.", "/images/Logger.png", ".NET Logger", "log | event | aggregate | app", 49.0 },
+                    { 6, "Powerful numerical methods for your .NET simulations. ", "/images/Numerics.png", ".NET Numerics", "power | num | simulat", 199.0 }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CartItems_BookId",
+                table: "CartItems",
+                column: "BookId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CartItems_CartId",
+                table: "CartItems",
+                column: "CartId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PurcahsedActivationCodes_CartItemId",
+                table: "PurcahsedActivationCodes",
+                column: "CartItemId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Sessions_CustomerId",
+                table: "Sessions",
+                column: "CustomerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Wishlist_AJAX_CustomerId",
+                table: "Wishlist_AJAX",
+                column: "CustomerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Wishlist_AJAX_itemId",
+                table: "Wishlist_AJAX",
+                column: "itemId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PurcahsedActivationCodes");
+
+            migrationBuilder.DropTable(
+                name: "Sessions");
+
+            migrationBuilder.DropTable(
+                name: "Wishlist_AJAX");
+
+            migrationBuilder.DropTable(
+                name: "CartItems");
+
+            migrationBuilder.DropTable(
+                name: "Customers");
+
+            migrationBuilder.DropTable(
+                name: "Books");
+
+            migrationBuilder.DropTable(
+                name: "Carts");
+        }
+    }
+}
